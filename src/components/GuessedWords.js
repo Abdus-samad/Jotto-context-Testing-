522f910902e69c93c67c86191f8cc99835ce03ad
Abdus-samad <abdussamadajao@gmail.com>
2021-09-30 16:31:04 +0100

    Tests and code for non-zero guessed word

--- src/components/GuessedWords.js
+++ src/components/GuessedWords.js
@@ -9,6 +9,27 @@ const GuessedWords = (props) => {
 				Try to guess the secret word!
 			</span>
 		);
+	} else {
+		const guessedWordsRows = props.guessedWords.map((word, index) => (
+			<tr  data-test='guessed-word' key={index}>
+				<td>{word.GuessedWord}</td>
+				<td>{word.letterMatchCount}</td>
+			</tr>
+		));
+		contents = (
+			<div data-test='guessed-words'>
+				<h3>Guessed Words</h3>
+				<table>
+					<thead>
+						<tr>
+							<th>Guess</th>
+							<th>Matching Letters</th>
+						</tr>
+					</thead>
+					<tbody>{guessedWordsRows}</tbody>
+				</table>
+			</div>
+		);
 	}
 
 	return <div data-test='component-guessed-words'>{contents}</div>;
@@ -17,7 +38,7 @@ const GuessedWords = (props) => {
 GuessedWords.propTypes = {
 	guessedWords: PropTypes.arrayOf(
 		PropTypes.shape({
-			guessedWords: PropTypes.string.isRequired,
+			guessedWord: PropTypes.string.isRequired,
 			letterMatchCount: PropTypes.number.isRequired,
 		})
 	).isRequired,
--- src/test/GuessedWords.test.js
+++ src/test/GuessedWords.test.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import {shallow} from 'enzyme';
-import {findByTestAttr, checkProps} from './testUtlis';
+import { shallow } from 'enzyme';
+import { findByTestAttr, checkProps } from './testUtlis';
 import GuessedWords from '../components/GuessedWords';
 
 const defaultProps = {
-	guessedWords: [{guessedWords: 'train', letterMatchCount: 3}],
+	guessedWords: [{guessedWord: 'train', letterMatchCount: 3}],
 };
 
 /**
@@ -27,14 +27,36 @@ describe('if there no words guessed', () => {
 	beforeEach(() => {
 		wrapper = setup({guessedWords: []});
 	});
-	test('render without error', () => {
+	test('renders without error', () => {
 		const component = findByTestAttr(wrapper, 'component-guessed-words');
 		expect(component.length).toBe(1);
 	});
-	test('render instructions to guess a word', () => {
+	test('renders instructions to guess a word', () => {
 		const instructions = findByTestAttr(wrapper, 'guess-instructions');
 		expect(instructions.text().length).not.toBe(0);
 	});
 });
 
-describe('if there are word guessed', () => {});
+describe('if there are word guessed', () => {
+	let wrapper;
+	const guessedWords = [
+		{guessedWord: 'train', letterMatchCount: 3},
+		{guessedWord: 'agile', letterMatchCount: 1},
+		{guessedWord: 'party', letterMatchCount: 5},
+	];
+	beforeEach(() => {
+		wrapper = setup({ guessedWords });
+	});
+	test('render without error', () => {
+		const component = findByTestAttr(wrapper, 'component-guessed-words');
+		expect(component.length).toBe(1);
+	});
+	test('renders "guessed words" section', () => {
+		const guessedWordsNode = findByTestAttr(wrapper, 'guessed-words');
+		expect(guessedWordsNode.length).toBe(1);
+	});
+	test('correct number of guessed words ', () => {
+		const guessedWordNodes = findByTestAttr(wrapper, 'guessed-word');
+		expect(guessedWordNodes.length).toBe(guessedWords.length);
+	});
+});
